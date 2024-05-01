a0a0cfa727b19cca11e5b0117c3d2e5acdc92c5d
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-04-30 19:43:05 -1000

    * Setting up the basic app structure.

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
-<script setup>
-import {collection} from 'firebase/firestore'
-
-const db = useFirestore()
-const units = useCollection(collection(db, 'units'))
-</script>
-
 <template>
-  <h1 class="text-3xl font-bold underline">
-    PANOPTES Dashboard
-  </h1>
-  <div>
-    <ul>
-      <li v-for="unit in units">
-        {{ unit.id }}
-      </li>
-    </ul>
-  </div>
+  <NuxtLayout>
+    <NuxtPage/>
+  </NuxtLayout>
 </template>
---
+++ pages/units.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {collection} from 'firebase/firestore'
+import DataView from 'primevue/dataview';
+
+const db = useFirestore()
+const units = useCollection(collection(db, 'units'))
+</script>
+
+<template>
+  <DataView :value="units">
+    <template #list="slotProps">
+      <div class="flex flex-wrap">
+        <div v-for="(unit, index) in slotProps.items" :key="unit.id" class="w-full p-3">
+          {{ unit.id }} {{ unit.name }}
+        </div>
+      </div>
+    </template>
+  </DataView>
+</template>
+
+<style scoped>
+
+</style>
