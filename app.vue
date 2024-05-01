113df90a0027b4fc8e154ffda53cb8a356516828
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-04-30 19:23:26 -1000

    Basic nuxt site and plugins installed.
    nuxt
    tailwind
    primevue
    apexcharts
    vuefire

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
+<script setup>
+import {collection} from 'firebase/firestore'
+
+const db = useFirestore()
+const units = useCollection(collection(db, 'units'))
+</script>
+
 <template>
+  <h1 class="text-3xl font-bold underline">
+    PANOPTES Dashboard
+  </h1>
   <div>
-    <NuxtWelcome />
+    <ul>
+      <li v-for="unit in units">
+        {{ unit.id }}
+      </li>
+    </ul>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    css: ['~/assets/css/main.css'],
+    modules: [
+        "nuxt-primevue",
+        "@nuxtjs/tailwindcss",
+        "nuxt-vuefire",
+    ],
+    vuefire: {
+        config: {
+            apiKey: "AIzaSyDNzSpa5589wVxoGXTKg-bmYbLWfwiT63M",
+            authDomain: "project-panoptes-01.firebaseapp.com",
+            projectId: "project-panoptes-01",
+            storageBucket: "project-panoptes-01.appspot.com",
+            messagingSenderId: "897068131063",
+            appId: "1:897068131063:web:f66ef8e66cae47937fe9f2",
+            measurementId: "G-FWPP66SEZ3"
+        }
+    },
+    primevue: {
+        options: {
+            unstyled: true
+        },
+        importPT: {from: '~/assets/css/presets/wind/'}      //import and apply preset
+    }
 })
