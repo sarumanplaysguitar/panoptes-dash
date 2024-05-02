fe9ec6766a73ca37e98fa2bedab3afc45923b9c2
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-01 16:57:46 -1000

    * Ugly implementation of power chart.

--- pages/units/[id].vue
+++ pages/units/[id].vue
@@ -1,24 +1,68 @@
 <script setup>
-import {collection, doc} from "firebase/firestore";
+import {collection, limit, orderBy, query} from "firebase/firestore";
 import Accordion from 'primevue/accordion';
 import AccordionTab from 'primevue/accordiontab';
-
 
 const route = useRoute()
 const db = useFirestore()
 
-const unitDoc = useDocument(doc(db, 'units', route.params.id), {wait: true})
-const metadata = useCollection(collection(db, 'units', route.params.id, 'metadata'), {wait: true})
+const metadataRef = collection(db, 'units', route.params.id, 'metadata')
+const metadata = useCollection(metadataRef, {wait: true})
 
-const power = computed(() => metadata.value.find((doc) => doc.id == 'power'))
-const weather = computed(() => metadata.value.find((doc) => doc.id == 'weather'))
-const safety = computed(() => metadata.value.find((doc) => doc.id == 'safety'))
-const state = computed(() => metadata.value.find((doc) => doc.id == 'state'))
-const observations = computed(() => metadata.value.find((doc) => doc.id == 'observations'))
+function getMetadataAsArray(collectionName, fieldName) {
+  const colRef = collection(db, 'units', route.params.id, 'metadata', collectionName, 'records')
+  const colQuery = useCollection(query(colRef, orderBy('received_time', 'desc'), limit(25)), {wait: true})
+
+  const data = computed(() => {
+    return colQuery.value.map(u => u[fieldName] ? [u.received_time.toDate(), parseFloat(u[fieldName])] : [u.received_time.toDate(), null])
+  })
+
+  return {name: fieldName, data: data}
+}
+
+const plotOptions = ref({
+  chart: {
+    id: 'power-plot'
+  },
+  title: {
+    text: 'Power readings'
+  },
+  animations: {
+    enabled: true,
+    easing: 'linear',
+    dynamicAnimation: {
+      speed: 500
+    }
+  },
+  xaxis: {
+    type: 'datetime'
+  },
+})
+
+const plotData = ref([
+  getMetadataAsArray('power', 'fans'),
+  getMetadataAsArray('power', 'weather_station'),
+  getMetadataAsArray('power', 'mount'),
+  getMetadataAsArray('power', 'unused1'),
+  getMetadataAsArray('power', 'unused3'),
+])
 </script>
 
 <template>
-  Hi {{ unitDoc?.id }}
+  Hi {{ route.params.id }}
+
+  <Card>
+    <template #content>
+      <ClientOnly> <!-- Cannot use SSR for apexcharts -->
+        <apexchart
+            :series="plotData"
+            :options="plotOptions"
+            width="500"
+            type="line"
+        ></apexchart>
+      </ClientOnly>
+    </template>
+  </Card>
 
   <Accordion :multiple="true">
     <AccordionTab v-for="md in metadata" :header="md.id">
