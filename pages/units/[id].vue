bc9967f07b18c02e5210ac23cda742b33af6cf64
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-01 19:31:26 -1000

    * Adding basic safety card.

--- pages/units/[id].vue
+++ pages/units/[id].vue
@@ -8,6 +8,12 @@ const db = useFirestore()
 
 const metadataRef = collection(db, 'units', route.params.id, 'metadata')
 const metadata = useCollection(metadataRef, {wait: true})
+
+const safety = computed(() => metadata.value.find(doc => doc.id == 'safety'))
+
+function getSeverity(val){
+  return val != undefined & val == true ? 'success' : 'danger'
+}
 </script>
 
 <template>
@@ -16,6 +22,23 @@ const metadata = useCollection(metadataRef, {wait: true})
   <Card>
     <template #content>
       <UnitMetadataPlot/>
+    </template>
+  </Card>
+
+  <Card>
+    <template #title>Safety</template>
+    <template #content>
+      <Tag :severity="getSeverity(safety?.ac_power)">AC_OK</Tag>
+      <br />
+      <Tag :severity="getSeverity(safety?.is_dark)">Dark</Tag>
+      <br />
+      <Tag :severity="getSeverity(safety?.free_space_root)">Free Space (/)</Tag>
+      <br />
+      <Tag :severity="getSeverity(safety?.free_space_images)">Free Space (images)</Tag>
+      <br />
+      <Tag :severity="getSeverity(safety?.good_weather)">Weather</Tag>
+      <br />
+      {{ safety.received_time.toDate().toLocaleString() }}
     </template>
   </Card>
 
