8f0b716209917c4a821dd01d7a80ca3f6962c343
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-02 18:22:04 -1000

    * Put safety metadata in a card.

--- components/UnitSafety.vue
+++ components/UnitSafety.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import {doc} from "firebase/firestore"
 
-import {useDayjs} from '#dayjs' // not need if you are using auto import
-const dayjs = useDayjs()
-
 const route = useRoute()
 const db = useFirestore()
 const safetyDoc = useDocument(doc(db, 'units', route.params.id, 'metadata', 'safety'), {wait: true})
@@ -15,18 +12,22 @@ function getSeverity(val) {
 </script>
 
 <template>
-  <br/>
-  <Tag :severity="getSeverity(safetyDoc?.ac_power)">AC_OK</Tag>
-  <br/>
-  <Tag :severity="getSeverity(safetyDoc?.is_dark)">Dark</Tag>
-  <br/>
-  <Tag :severity="getSeverity(safetyDoc?.free_space_root)">Free Space (/)</Tag>
-  <br/>
-  <Tag :severity="getSeverity(safetyDoc?.free_space_images)">Free Space (images)</Tag>
-  <br/>
-  <Tag :severity="getSeverity(safetyDoc?.good_weather)">Weather</Tag>
-  <br/>
-  {{ $dayjs().to($dayjs(safetyDoc?.received_time.toDate()).utc()) }}
+  <Card>
+    <template #content>
+      <br/>
+      <Tag :severity="getSeverity(safetyDoc?.ac_power)">AC_OK</Tag>
+      <br/>
+      <Tag :severity="getSeverity(safetyDoc?.is_dark)">Dark</Tag>
+      <br/>
+      <Tag :severity="getSeverity(safetyDoc?.free_space_root)">Free Space (/)</Tag>
+      <br/>
+      <Tag :severity="getSeverity(safetyDoc?.free_space_images)">Free Space (images)</Tag>
+      <br/>
+      <Tag :severity="getSeverity(safetyDoc?.good_weather)">Weather</Tag>
+      <br/>
+      {{ $dayjs().to($dayjs(safetyDoc?.received_time.toDate()).utc()) }}
+    </template>
+  </Card>
 </template>
 
 <style scoped>
