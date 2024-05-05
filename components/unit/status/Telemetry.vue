6f866467719dd5f27faaf6d27b8faa05fdd2f958
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-04 19:48:01 -1000

    * Put telemetry plot in dash

--- components/unit/status/Telemetry.vue
+++ components/unit/status/Telemetry.vue
@@ -4,16 +4,8 @@
 
 <template>
   <div class="rounded-md bg-neutral-800 p-4 text-neutral-500 col-span-1">
-    <p class="text-sm uppercase pb-0">
-                        <span class="material-symbols-outlined text-sm align-bottom pr-1">
-                            timeline
-                        </span>
-      TELEMETRY
-    </p>
-    <p>Power (AC, Weather station, Fan, Mount) - last updated</p>
-    <p>Safety (AC Power, Weather, Storage, etc) - last updated</p>
-    <p>State (ie. slewing)</p>
-    <p>Exposure indicators, for primary/secondary cameras</p>
+    <p class="text-sm uppercase pb-0">TELEMETRY</p>
+    <UnitMetadataPlot/>
   </div>
 </template>
 
