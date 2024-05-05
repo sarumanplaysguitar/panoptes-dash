d08eacef2730c55361bc59df81ae2edc7e396607
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-05 08:40:46 -1000

    * Bigger telemetry plot

--- components/unit/status/Telemetry.vue
+++ components/unit/status/Telemetry.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="rounded-md bg-neutral-800 p-4 text-neutral-500 col-span-1">
+  <div class="rounded-md bg-neutral-800 p-4 text-neutral-500 col-span-1 md:col-span-2">
     <p class="text-sm uppercase pb-0">TELEMETRY</p>
     <UnitMetadataPlot/>
   </div>
