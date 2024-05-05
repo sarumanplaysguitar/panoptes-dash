288c95f10db67b7bfc7e0ece6a20836ed481e4c8
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-04 14:25:41 -1000

    * Sort of clean up the Unit Selector component. Styling needs work.

--- components/UnitSelector.vue
+++ components/UnitSelector.vue
@@ -1,48 +1,33 @@
 <script setup>
+import {collection} from "firebase/firestore";
+import Avatar from 'primevue/avatar';
 
+const db = useFirestore()
+
+const units = useCollection(collection(db, 'units'), {once: true})
 </script>
 
 <template>
-    <!-- <div class="bg-neutral-900 flex flex-row gap-3 p-2 md:flex-col md:h-screen md:w-[3.6rem] items-center overflow-y-auto">
-        <div class="-rotate-12 font-['Oxanium'] text-xs mt-[0.1rem] ml-2 md:mt-2 md:ml-0 bg-neutral-600 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-300">012</div>
-        <div class="-rotate-12 font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-850 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600 ring-2 ring-inset ring-neutral-800">000</div>
-        <div class="-rotate-12 font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-850 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600 ring-2 ring-inset ring-neutral-800">007</div>
-        <div class="-rotate-12 font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-850 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600 ring-2 ring-inset ring-neutral-800">026</div>
-        <div class="-rotate-12 font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-850 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600 ring-2 ring-inset ring-neutral-800">008</div>
-    </div> -->
-
-    <div class="bg-neutral-900 flex flex-row gap-3 p-2 h-[3.5m] md:flex-col md:h-screen md:w-[3.6rem] items-center overflow-y-auto">
-        <!-- <div class="font-['Oxanium'] text-xs mt-[0.1rem] ml-2 md:mt-2 md:ml-0 bg-neutral-500 min-w-9 min-h-9 rounded-full flex items-top justify-center text-neutral-300 pt-1" :style="backgroundStyle">012</div> -->
-        <div class="relative inline-block">
-            <!-- <div class="font-['Oxanium'] text-xs mt-[0.1rem] ml-2 md:mt-2 md:ml-0 bg-neutral-500 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-200 pt-1 ring-2 ring-offset-2 ring-offset-neutral-900 ring-neutral-700"><span class="animate-avg-pulse">012</span></div> -->
-            <div class="font-['Oxanium'] text-xs mt-[0.1rem] ml-2 md:mt-2 md:ml-0 bg-neutral-500 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-200 pt-1 \">012</div>
-
-            <span class="align-self-center inline-flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-900 bg-neutral-300 absolute -bottom-0.5 -right-0.5"></span>
-            <!-- <span class="w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-800 bg-neutral-400 absolute -bottom-0.5 -right-0.5"></span> -->
-        </div>
-        <div class="relative inline-block">
-            <div class="font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-300">000</div>
-            <!-- <span class="align-self-center animate-modified-ping inline-flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-900 bg-neutral-300 absolute -bottom-0.5 -right-0.5"></span> -->
-            <span class="align-self-center inline-flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-900 bg-neutral-300 absolute -bottom-0.5 -right-0.5"></span>
-
-            <!-- <span class="align-self-center flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-800 bg-neutral-400 absolute -bottom-0.5 -right-0.5"></span> -->
-            <!-- <span class="align-self-center flex w-[0.8rem] h-[0.8rem] rounded-full bg-neutral-400 bg-opacity-0 absolute -bottom-0.5 -right-0.5"></span> -->
-
-        </div>
-
-        <div class="relative inline-block">
-            <div class="font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600">007</div>
-            <!-- <span class="align-self-center animate-modified-ping inline-flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-900 bg-neutral-300 absolute -bottom-0.5 -right-0.5"></span> -->
-            <!-- <span class="font-['Oxanium'] w-[0.8rem] h-[0.8rem] absolute bottom-1 -right-0.5 text-neutral-500">z</span> -->
-
-            <!-- <span class="align-self-center flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-800 bg-neutral-400 absolute -bottom-0.5 -right-0.5"></span> -->
-            <!-- <span class="align-self-center flex w-[0.8rem] h-[0.8rem] rounded-full bg-neutral-400 bg-opacity-0 absolute -bottom-0.5 -right-0.5"></span> -->
-
-        </div>
-
-
-        <!-- <div class="font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600">007</div> -->
-        <div class="font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600">026</div>
-        <div class="font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-600">008</div>
+  <div class="avatar_bg">
+    <div class="relative inline-block" v-for="unit in units">
+      <Avatar :label="unit.unit_id.slice(3)" class="avatar" shape="circle">
+        {{ unit.unit_id.slice(3) }}
+        <span class="avatar_active"></span>
+      </Avatar>
     </div>
+  </div>
 </template>
+
+<style scoped>
+.avatar_bg {
+  @apply bg-neutral-900 flex flex-row gap-3 p-2 h-[3.5m] md:flex-col md:h-screen md:w-[3.6rem] items-center overflow-y-auto
+}
+
+.avatar {
+  @apply font-['Oxanium'] text-xs mt-[0.1rem] bg-neutral-800 min-w-9 min-h-9 rounded-full flex items-center justify-center text-neutral-300
+}
+
+.avatar_active {
+  @apply inline-flex w-[0.8rem] h-[0.8rem] rounded-full border-2 border-neutral-900 bg-neutral-300 absolute -bottom-0.5 -right-0.5
+}
+</style>
