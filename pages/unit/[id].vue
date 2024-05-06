a1d4b4883ec62ff57f614d1f4fa4f4ad065fd621
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-05 14:57:28 -1000

    * Clean up how columns are made by default.

--- pages/unit/[id].vue
+++ pages/unit/[id].vue
@@ -8,16 +8,16 @@ const unitDoc = useDocument(() => unitsStore.getUnitDoc(route.params.id))
 
 <template>
   <!-- Main Layout -->
-  <div class="min-h-screen flex flex-col md:grid md:grid-cols-[3.6rem_18rem_1fr] md:grid-rows-1">
-    <div class="h-14 md:h-auto md:col-span-1 md:row-span-1">
+  <div class="flex flex-row min-h-screen">
+    <div class="basis-1/16">
       <UnitSelector/>
     </div>
 
-    <div class="md:h-auto md:col-span-1 md:row-span-1">
+    <div class="basis-3/16">
       <LazyUnitAboutSelectedUnit :unit="unitDoc"/>
     </div>
 
-    <div class="md:col-span-1 md:row-span-1 overflow-auto">
+    <div class="basis-3/4">
       <LazyUnitDashboardContent :unit="unitDoc"/>
     </div>
 
