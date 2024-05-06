b2045e12379b9f24b7dec631db420a462827edac
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-05 19:35:23 -1000

    * Home page wrapped in cards.

--- pages/index.vue
+++ pages/index.vue
@@ -41,44 +41,49 @@ const plotSeries = ref([{
 </script>
 
 <template>
-  <Card>
-    <template #content>
-      <ClientOnly> <!-- Cannot use SSR for apexcharts -->
-        <apexchart
-            :series="plotSeries"
-            :options="plotOptions"
-            width="500"
-            type="bar"
-        ></apexchart>
-      </ClientOnly>
-    </template>
-  </Card>
-
-  <Card>
-    <template #content>
-      <DataTable
-          stripedRows
-          showGridlines
-          size="small"
-          :value="units"
-          tableStyle="min-width: 50rem">
-        <Column field="id" header="ID" sortable>
-          <template #body="slotProps">
-            <NuxtLink :to="`/unit/${slotProps.data.id}`">{{ slotProps.data.id }}</NuxtLink>
-          </template>
-        </Column>
-        <Column field="name" header="Name" sortable></Column>
-        <Column field="latitude" header="Lat (deg)">
-          <template #body="slotProps">
-            {{ slotProps.data.latitude.toPrecision(5) }}°
-            {{ slotProps.data.longitude.toPrecision(5) }}°
-            {{ slotProps.data.elevation }}m
-          </template>
-        </Column>
-        <Column field="num_images" header="# Images" sortable></Column>
-      </DataTable>
-    </template>
-  </Card>
+  <div class="flex flex-row">
+    <div class="basis-3/4">
+      <Card>
+        <template #content>
+          <DataTable
+              stripedRows
+              showGridlines
+              size="small"
+              :value="units"
+              tableStyle="min-width: 50rem">
+            <Column field="id" header="ID" sortable>
+              <template #body="slotProps">
+                <NuxtLink :to="`/unit/${slotProps.data.id}`">{{ slotProps.data.id }}</NuxtLink>
+              </template>
+            </Column>
+            <Column field="name" header="Name" sortable></Column>
+            <Column field="latitude" header="Lat (deg)">
+              <template #body="slotProps">
+                {{ slotProps.data.latitude.toPrecision(5) }}°
+                {{ slotProps.data.longitude.toPrecision(5) }}°
+                {{ slotProps.data.elevation }}m
+              </template>
+            </Column>
+            <Column field="num_images" header="# Images" sortable></Column>
+          </DataTable>
+        </template>
+      </Card>
+    </div>
+    <div class="basis-1/4">
+      <Card>
+        <template #content>
+          <ClientOnly> <!-- Cannot use SSR for apexcharts -->
+            <apexchart
+                :series="plotSeries"
+                :options="plotOptions"
+                width="500"
+                type="bar"
+            ></apexchart>
+          </ClientOnly>
+        </template>
+      </Card>
+    </div>
+  </div>
 </template>
 
 <style scoped>
