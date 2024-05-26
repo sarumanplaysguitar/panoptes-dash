deb4514fe211627a529271f06177dd596030fd9a
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-26 07:37:07 -1000

    Remove old styling.

--- components/unit/UnitDashboardContent.vue
+++ components/unit/UnitDashboardContent.vue
@@ -12,11 +12,5 @@
 </template>
 
 <style scoped>
-.tab-holder {
-  @apply bg-neutral-900 flex flex-col py-2 px-1 md:h-screen
-}
 
-.tab-panel {
-  @apply grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 w-full p-1 pb-2 mt-1
-}
 </style>
