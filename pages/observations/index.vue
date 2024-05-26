0e63794e51dd8005467475c35e5e5786dc4ce1d9
Wilfred Tyler Gee <wtylergee@gmail.com>
2024-05-26 07:36:21 -1000

    Remove unnecessary loading of `observations`

--- pages/observations/index.vue
+++ pages/observations/index.vue
@@ -1,17 +1,12 @@
 <script setup lang="ts">
-import {usePendingPromises} from 'vuefire'
 
-const unitsStore = useUnitsStore()
-const observations = computed(() => unitsStore.currentObservations ? unitsStore.currentObservations : [])
-
-onServerPrefetch(() => usePendingPromises())
 </script>
 
 <template>
   <Card>
     <template #header>Observations</template>
     <template #content>
-      <RecentObservationsTable :observations="observations"/>
+      <RecentObservationsTable />
     </template>
   </Card>
 </template>
