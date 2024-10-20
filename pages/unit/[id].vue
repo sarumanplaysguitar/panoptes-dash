a93235bbffff18b62cc559d76300586a938633be
Therese <sarumanplaysguitar@gmail.com>
2024-10-20 15:16:55 -0700

    Update [id].vue
    working on the SWEEP

--- pages/unit/[id].vue
+++ pages/unit/[id].vue
@@ -1,41 +1,74 @@
 <script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const isPanelExpanded = ref(false);
+
+  const togglePanel = () => {
+    isPanelExpanded.value = !isPanelExpanded.value;
+  };
 </script>
 
 <template>
-  <!-- Main Layout -->
-  <!-- <div class="flex flex-row min-h-screen">
-    <div class="basis-1/16">
-      <UnitSelector/>
-    </div>
-
-    <div class="basis-3/16">
-      <Panel header="Unit Info">
-      <UnitAboutSelectedUnit/>
-      </Panel>
-    </div>
-
-    <div class="basis-3/4">
-      <UnitDashboardContent/>
-    </div>
-    <RouterView/>
-  </div> -->
-  <div class="min-h-screen flex flex-col md:grid md:grid-cols-[3.6rem_18rem_1fr] md:grid-rows-1 bg-neutral-900">
-    <div class="h-14 md:h-auto md:col-span-1 md:row-span-1">
+  <div class="min-h-screen flex flex-col md:flex-row bg-neutral-900">
+    <div class="h-14 md:h-auto md:w-[3.6rem]">
       <UnitSelector />
     </div>
-    
-    <div class="md:h-auto md:col-span-1 md:row-span-1">
-      <UnitAboutSelectedUnit/>
+
+    <!-- About selected unit (Expandable panel) -->
+    <div
+      class="md:h-auto duration-500 ease bg-blue-300"
+      :style="{
+        width: isPanelExpanded ? '100%' : '18rem',
+        minWidth: '18rem',
+        flexGrow: isPanelExpanded ? 1 : 0,
+        transitionDelay: isPanelExpanded ? '300ms' : '0ms'  // delay for panel width change
+      }">
+      <p class="text-white">Unit About Selected Unit</p>
+      <button
+        class="bg-blue-500 text-white p-2 mt-4"
+        @click="togglePanel"
+      >
+        {{ isPanelExpanded ? 'Collapse Panel' : 'Expand Panel' }}
+      </button>
     </div>
-    
-    <div class="md:col-span-1 md:row-span-1 overflow-auto">
-      <UnitDashboardContent />
-    </div>
-    
+
+    <!-- Unit dashboard content (collapsible) -->
+    <transition name="expand-panel">
+      <div
+        class="overflow-auto transition-opacity duration-500 ease"
+        :style="{
+          width: isPanelExpanded ? '0' : 'auto',
+          opacity: isPanelExpanded ? 0 : 1,
+          flexGrow: isPanelExpanded ? 0 : 1,
+          transitionDelay: isPanelExpanded ? '0ms' : '300ms'  // Delay for dashboard content collapse when shrinking
+        }">
+        <UnitDashboardContent />
+      </div>
+    </transition>
+
     <RouterView />
   </div>
 </template>
 
 <style scoped>
+.expand-panel-enter-active,
+.expand-panel-leave-active {
+  transition: opacity .3s ease;
+}
 
+.expand-panel-enter-from {
+  opacity: 0;
+}
+
+.expand-panel-enter-to {
+  opacity: 1;
+}
+
+.expand-panel-leave-from {
+  opacity: 1;
+}
+
+.expand-panel-leave-to {
+  opacity: 0;
+}
 </style>
